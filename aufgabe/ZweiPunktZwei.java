a8bd77506db8a1429c88e65ce9cd6629639008e7
Rashad Khoshnow <rashad.khoshnow@gmail.com>
2020-08-31 14:10:29 +0200

    Autoformatiert

--- aufgabe/ZweiPunktZwei.java
+++ aufgabe/ZweiPunktZwei.java
@@ -5,9 +5,10 @@ public class ZweiPunktZwei {
 
         System.out.println("number is: " + number);
 
-        if(number % 10 == 0){
+        if (number % 10 == 0) {
             System.out.println("Volle");
-        }   if (number % 2 == 0) {
+        }
+        if (number % 2 == 0) {
             System.out.println("gerade");
         } else {
             System.out.println("ungerade");
@@ -17,8 +18,8 @@ public class ZweiPunktZwei {
         if (luckyNumber == 2) {
             System.out.println("That is my lucky number");
         }
-            if (luckyNumber > 9 && luckyNumber <= 99 || luckyNumber <-9 && luckyNumber >=-99) {
-                System.out.println("zweistellig");
-            }
+        if (luckyNumber > 9 && luckyNumber <= 99 || luckyNumber < -9 && luckyNumber >= -99) {
+            System.out.println("zweistellig");
         }
     }
+}
